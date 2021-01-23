09dc210985d118ba49b8f4dff75a6522c6f47fe9
tevko <timevko@gmail.com>
2021-01-23 17:27:10 -0500

    initial setup

---
+++ .bin/publish.sh
@@ -0,0 +1,7 @@
+#!/.bin/
+echo "Sending up to Github Pages 🚀" 
+rm -rf dist
+npx parcel build index.html --public-url ./ --no-cache 
+git add dist 
+git commit -m "update latest build" 
+git push origin `git subtree split --prefix dist master`:gh-pages --force 
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Countries from './components/Countries';
+
+ReactDOM.render(
+  <Countries />,
+  document.querySelector('main')
+);
